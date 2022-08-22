4bc015d55cf138f25625d0a0ad4e944abc1a2d67
Jaime Ansorena <47193717+s7argaze@users.noreply.github.com>
2022-08-21 21:23:07 -0400

    listado1

--- Listado1/problem_F.cpp
+++ Listado1/problem_F.cpp
@@ -5,26 +5,45 @@ class WordFindPuzzle{
 private:
     int solucion = 0;
     vector<string> board;
+    unordered_map<string,pair<bool,bool>> map;
 public:
     void busqueda(int dir, string word, int pos, int i, int j){
-        solucion++;
+        if(pos == word.length()){
+            if(dir == 1 || dir == 3) map[word].first = true;
+            else map[word].second = true;
+        }
+        else{
+            if(dir == 1 && j<board[0].length() && board[i][j] == word[pos]) 
+                busqueda(1,word,pos+1,i,j+1);
+            if(dir == 2 && i<board.size() && board[i][j] == word[pos]) 
+                busqueda(2,word,pos+1,i+1,j);
+            if(dir == 3 && j>=0 && board[i][j] == word[pos]) 
+                busqueda(3,word,pos+1,i,j-1);
+            if(dir == 4 && i>=0 && board[i][j] == word[pos]) 
+                busqueda(4,word,pos+1,i-1,j);
+        }
     }
 
     int solveWordFindPuzzle(vector<string> words, vector <string> board){
         this->board = board;
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].length(); j++){
+        int ancho = board[0].length();
+        int largo = board.size();
+        for(int i = 0; i < largo; i++){
+            for(int j = 0; j < ancho; j++){
                 for(auto x : words){
                     if(x[0] == board[i][j]){
-                        if(j < board[0].length()-1) busqueda(1,x,1,i,j++);
-                        if(i < board.size()-1) busqueda(2,x,1,i++,j);
-                        if(j > 0) busqueda(3,x,1,i,j--);
-                        if(i > 0) busqueda(4,x,1,i--,j);
+                        if(j<ancho) busqueda(1,x,0,i,j);
+                        if(i<largo) busqueda(2,x,0,i,j);
+                        if(j>=0) busqueda(3,x,0,i,j);
+                        if(i>=0) busqueda(4,x,0,i,j);
                     }
                 }
             }
         }
-
+        for(auto x : map){
+            if (x.second.first && x.second.second) solucion++;
+            cout << x.first << " " << x.second.first << " " << x.second.second << endl;
+        }       
         return solucion;
     }
 };
@@ -32,10 +51,10 @@ public:
 int main(){
     vector<string> words = {"park", "kim", "lee", "choi"};
     vector<string> board = {"lxamal",
-                            "alkime",
-                            "parkpe",
-                            "lyaeel",
-                            "aypepl"};
+ "alkime",
+ "parkpe",
+ "lyaeel",
+ "aypepl"};
     WordFindPuzzle wf;
     cout << wf.solveWordFindPuzzle(words,board);
     return 0;
