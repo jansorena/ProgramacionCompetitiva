dcf505eb6dd4897cc26fb566718d8406025544a5
Jaime Ansorena <jaimeansorenac@gmail.com>
2023-04-04 22:51:47 -0400

    ejercicio B

--- 2023-Listado1/problem_B.cpp
+++ 2023-Listado1/problem_B.cpp
@@ -2,96 +2,82 @@
 using namespace std;
 
 int main(){
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+
 	int n,m,b,p;
 
 	while(cin >> n >> m >> b >> p){
 		vector<pair<int,int>> adj[n];
-		vector<int> bancos, policias;
-		int n1, m1, dis;
-
-		while(m--){
-			cin >> n1 >> m1 >> dis;
-			adj[n1].push_back({m1,dis});
-			adj[m1].push_back({n1,dis});
+		vector<int> bancos,policias;
+		int max_dis[n];
+		for (int i = 0; i < n; ++i) max_dis[i] = -1;
+		
+		for (int i = 0; i < m; ++i){
+			int v1,v2,v3; cin >> v1 >> v2 >> v3;
+			adj[v1].push_back({v2,v3});
+			adj[v2].push_back({v1,v3});
+		}
+		for (int i = 0; i < b; ++i){
+			int aux; cin >> aux;
+			bancos.push_back(aux);
+		}
+		for (int i = 0; i < p; ++i){
+			int aux; cin >> aux;
+			policias.push_back(aux);
 		}
 
-		while(b--){
-			int aux1;
-			cin >> aux1;
-			bancos.push_back(aux1);
-		}
-		while(p--){
-			int aux1;
-			cin >> aux1;
-			policias.push_back(aux1);
-		}
-
-		int bestDistancia[n] = {INT_MAX};
+		// dijkstra 
+		
+		priority_queue<pair<int,int>> q;
+		int distancia[n];
+		bool visitado[n];
+		for (int i = 0; i < n; i++) distancia[i] = INT_MAX;
+		for (int i = 0; i < n; i++) visitado[i] = false;
 
 		for(auto x : policias){
-			priority_queue<pair<int,int>> q;
-			int distancia[n];
-	        for (int i = 0; i < n; i++) distancia[i] = INT_MAX;
-	        bool procesado[n] = {false};
+			distancia[x] = 0;
+			q.push({0,x});
+		}
 
-	        distancia[x] = 0; // distancia del primer nodo
-	        
-	        q.push({0,x}); // agrega el primero nodo a la pq
-
-	        while(!q.empty()){
-	            int a = q.top().second;
-	            q.pop();
-	            if(procesado[a]) continue; // si el nodo ya fue visitado
-	            procesado[a] = true;
-	            for (auto y : adj[a]){
-	                int b = y.first;
-	                int l = y.second;
-	                if(distancia[a]+l < distancia[b]){ //si la distancia del nodo a al nodo b es menor q la distancia que b
-	                    distancia[b] = distancia[a]+l;
-	                    q.push({-distancia[b],b}); //simbolo negativo para ordenar de mayor a menor
-	                }
+		while(!q.empty()){
+			int a = q.top().second;
+	        q.pop();
+	        if(visitado[a]) continue; 
+	        visitado[a] = true;
+	        for (auto y : adj[a]){
+	            int b = y.first;
+	            int l = y.second;
+	            if(distancia[a]+l < distancia[b]){
+	                distancia[b] = distancia[a]+l;
+	                q.push({-distancia[b],b}); 
 	            }
 	        }
-
-	        for(int i = 0; i < bancos.size(); i++){
-	        	if(distancia[bancos[i]] != INT_MAX && distancia[bancos[i]] > bestDistancia[bancos[i]]) bestDistancia[bancos[i]] = distancia[bancos[i]];
-	        }
 		}
 
-		map<int,vector<int>> mymap;
-
-		for (int i = 0; i < bancos.size(); ++i)
-		{
-			mymap[bestDistancia[bancos[i]]].push_back(bancos[i]);
+		for(auto banco : bancos) max_dis[banco] = max(max_dis[banco],distancia[banco]);
+		
+		vector<int> ans;
+		int max_value = 0;
+		for (int i = 0; i < bancos.size(); ++i){
+			max_value=max(max_value,max_dis[bancos[i]]);
 		}
 
-		auto it1 = mymap.begin();
-		auto it2 = mymap.rbegin();
-
-
-		if(it1->first == 0){
-			cout << it1->second.size() << " " << "*" << endl;
-			sort(it1->second.begin(), it1->second.end());
-			for(auto x : it1->second){
-				cout << x << " ";
-			}
-			cout << endl;
-		}else{
-			cout << it2->second.size() << " " << it2->first << endl;
-			sort(it2->second.begin(), it2->second.end());
-			for(auto x : it2->second){
-				cout << x << " ";
-			}
-			cout << endl;
+		for (int i = 0; i < bancos.size(); ++i){
+			if(max_dis[bancos[i]] == max_value) ans.push_back(bancos[i]);
 		}
+
+		if(max_value != INT_MAX) cout << ans.size() << " " << max_value << endl;
+		else cout << ans.size() << " *" << endl;
+
+		sort(ans.begin(),ans.end());
+		cout << ans[0];
+		for(int i = 1; i < ans.size(); ++i) cout << " "<< ans[i];
+		cout << endl;
 
 	}
-
-	
-
 
 	return 0;
 }
